2f06b38d4c6fa49990227a54f40e617213fcfefb
Khailas R <khailas303@gmail.com>
2021-09-26 21:30:18 +0530

    Updated

--- custom_action.py
+++ custom_action.py
@@ -1,17 +1,16 @@
 import argparse
 
 
+# The nargs (No. of arguments) accepts a couple of extra special parameters. 
+# If you want the argument to accept all of the parameters, you can use * which will return all parameters if present, 
+# or empty list if none. Positional arguments are determined by the position specified.
+
 class verboseStore(argparse.Action):
     def __init__(self, option_strings, dest, nargs=None, **kwargs):
-        
-        # The nargs (No. of arguments) accepts a couple of extra special parameters. 
-        # If you want the argument to accept all of the parameters, you can use * which will return all parameters if present, 
-        # or empty list if none. Positional arguments are determined by the position specified.
         
         if nargs is not None:
             raise ValueError('nargs not allowed')
         super(verboseStore, self).__init__(option_strings, dest, **kwargs)
-        
         # super keyword in line ​10 allows the child class to access the parent class's init() property. 
         # In other words, super() allows you to build classes that easily extend the functionality of previously built classes without implementing their functionality again.
         
