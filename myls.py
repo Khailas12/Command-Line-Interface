2383ce04fa8d32d010c52be779b5282ad5486cd5
Khailas R <khailas303@gmail.com>
2021-09-26 19:33:36 +0530

    initial commit

--- myls.py
+++ myls.py
@@ -1,21 +1,26 @@
+import argparse
 import os
 import sys
 
 
-if len(sys.argv) == 2:
-    print('You have specified too many arguments')
-    sys.exit()
-    
-if len(sys.argv) < 2:
-    print('You need to specify the path to be listed')
-    sys.exit()
-    
-input_path = sys.argv[1]
+# creating parser
+my_parser = argparse.ArgumentParser(description='List the content of a folder')
+
+
+# adding arguments
+my_parser.add_argument(
+    'Path',
+    metavar='path',
+    type=str,
+    help='the path of list'
+)
+
+args = my_parser.parse_args()   # Executing the parse.args() method
+
+input_path = args.Path
 
 if not os.path.isdir(input_path):
     print('The path specified does not exist')
     sys.exit()
     
 print('\n'.join(os.listdir(input_path)))
-
-
